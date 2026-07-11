a3888dc320a313308d54a96e46d5f163f3b85bce
Harshit Vaishnav <vaishnavharshit01@gmail.com>
2026-07-11 22:40:26 +0530

    added revrse word in a string

---
+++ Arrays/reverse_word_in_string.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+    string reverseWords(string s)
+    {
+
+        reverse(s.begin(), s.end());
+
+        int n = s.length();
+        int i = 0, l = 0, r = 0;
+
+        while (i < n)
+        {
+            // skip spaces
+            while (i < n && s[i] == ' ')
+                i++;
+
+            // copy word
+            while (i < n && s[i] != ' ')
+                s[r++] = s[i++];
+
+            // reverse copied word
+            if (l < r)
+            {
+                reverse(s.begin() + l, s.begin() + r);
+
+                // add space only if next word exists
+                while (i < n && s[i] == ' ')
+                    i++;
+
+                if (i < n)
+                    s[r++] = ' ';
+
+                l = r;
+            }
+        }
+
+        return s.substr(0, r);
+    }
+};
